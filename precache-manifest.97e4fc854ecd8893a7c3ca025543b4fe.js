5b704a8eb1e7064f0942b342582bff8d77786b87
Fyodor Prokofiev <9195737+fprokofiev@users.noreply.github.com>
2019-11-23 23:26:38 +0300

    Updates

---
+++ precache-manifest.97e4fc854ecd8893a7c3ca025543b4fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e502f30514b9a1f1ff93a38a46b0d6",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "558c022e93c15c1f69ed",
+    "url": "/face-recognition-brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b0fdacadf255f11d3519",
+    "url": "/face-recognition-brain/static/css/main.dd3e540a.chunk.css"
+  },
+  {
+    "revision": "558c022e93c15c1f69ed",
+    "url": "/face-recognition-brain/static/js/2.40cbf4c8.chunk.js"
+  },
+  {
+    "revision": "b0fdacadf255f11d3519",
+    "url": "/face-recognition-brain/static/js/main.e721d62e.chunk.js"
+  },
+  {
+    "revision": "50474b6f342b6cf8d4ab",
+    "url": "/face-recognition-brain/static/js/runtime-main.fceaeb89.js"
+  },
+  {
+    "revision": "678569f2470764fdd5602849adf4e5bd",
+    "url": "/face-recognition-brain/static/media/Logo.678569f2.png"
+  }
+]);
