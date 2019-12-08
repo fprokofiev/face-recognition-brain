344a302581af2f10bb6da0dea7252de532ad9d6a
Fyodor Prokofiev <9195737+fprokofiev@users.noreply.github.com>
2019-12-08 17:54:03 +0300

    update

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -16,7 +16,7 @@ class SignIn extends Component {
 		this.setState({signInPassword: event.target.value})
 	}
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3001/signin', {
+		fetch('https://fierce-sands-57090.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
